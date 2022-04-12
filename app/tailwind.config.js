352ad671c54a007c0c771ac7bbf7894fdeae0495
nkosakul <nkosakul@3pc.de>
2022-04-12 21:58:59 +0200

    fix(app): let vite build tailwind classes from ui-lib as well

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,5 +1,10 @@
 const tailwindPreset = require('config/tailwind.config');
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    // TODO: try to find another way to include tailwind classes from ui-lib
+    '../packages/ui-lib/src/components/**/*.{vue,js,ts,jsx,tsx}',
+  ],
   ...tailwindPreset,
 };
